c63d4e174fe01c366d58f60b449663c74987a84c
DTDgava <nazarometyukh@gmai.com>
2022-11-02 16:01:43 +0200

    1.04

--- Jam_Game/Assets/Scripts/Flashlight.cs
+++ Jam_Game/Assets/Scripts/Flashlight.cs
@@ -10,25 +10,74 @@ public class Flashlight : MonoBehaviour
     public float maxValueBattery = 100f;
     public float currentValueBattery = 70f;
 
+    public float SpotAngleFlashLight = 50f;
+
+
+    public float SpeedBatteryLose;
+    public float SpeedBatteryLoseGhost;
+
     private void Update()
     {
         TurnFlashLight();
+        BatteryUpdate();
     }
 
+
+    void BatteryUpdate()
+    {
+        if (currentValueBattery > 0)
+        {
+            if (standartLight.enabled == true)
+            {
+                currentValueBattery -= Time.deltaTime * SpeedBatteryLose;
+            }
+            else if (ghostLight.enabled == true)
+            {
+                currentValueBattery -= Time.deltaTime * SpeedBatteryLoseGhost;
+            }
+        }
+        else if(currentValueBattery <= 0)
+        {
+            currentValueBattery = 0;
+            standartLight.enabled = false;
+            ghostLight.enabled = false;
+            ghostLight.spotAngle = 1;
+        }
+    }
     void TurnFlashLight()
     {
-        if(FlashlightObject.activeSelf == true)
+        if(FlashlightObject.activeSelf == true && currentValueBattery > 0)
         {
             if (Input.GetMouseButtonDown(1))
             {
-                standartLight.enabled = true;
-                ghostLight.enabled = false;
+                if (ghostLight.enabled == true)
+                {
+                    standartLight.enabled = true;
+                    ghostLight.enabled = false;
+                    ghostLight.spotAngle = 1;
+                }
+                else if (standartLight.enabled == true)
+                {
+                    standartLight.enabled = false;
+                    ghostLight.enabled = true;
+                    ghostLight.spotAngle = SpotAngleFlashLight;
+                }
             }
 
-            if (Input.GetKeyDown(KeyCode.F)){
-              
-                standartLight.enabled = false;
-                ghostLight.enabled = true;
+            else if (Input.GetKeyDown(KeyCode.F))
+            {
+                if (standartLight.enabled == true | ghostLight.enabled == true)
+                {
+                    standartLight.enabled = false;
+                    ghostLight.enabled = false;
+                    ghostLight.spotAngle = 1;
+                }
+                else if(standartLight.enabled == false | ghostLight.enabled == false)
+                {
+                    standartLight.enabled = true;
+                    ghostLight.enabled = false ;
+                    ghostLight.spotAngle = 1;
+                }
             }
         }
     }
